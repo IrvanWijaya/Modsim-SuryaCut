264dd4078c5097b399db9901c81441d2a6373c29
Cornelius David Herianto <cornelius.david97@gmail.com>
2018-05-01 15:28:17 +0700

    add model class

---
+++ src/suryacut/model/Pelanggan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package suryacut.model;
+
+/**
+ *
+ * @author CorneliusDavid
+ */
+public class Pelanggan {
+    private String nama;
+    private char gender;
+
+    public Pelanggan(String nama, char gender) {
+        this.nama = nama;
+        this.gender = gender;
+    }
+    
+    
+}
